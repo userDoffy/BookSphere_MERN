a0dbdab4ad196755ee6545204875a53593adb7f7
userDoffy <elitedoffy@gmail.com>
2025-05-18 21:37:57 +0545

    chanegs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,11 +7,9 @@ import store from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </StrictMode>
 );

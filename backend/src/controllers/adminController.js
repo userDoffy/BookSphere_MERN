de12cd254efeffde4e352d67179e998f0e75108c
userDoffy <elitedoffy@gmail.com>
2025-05-21 10:24:40 +0545

    admin page

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -42,8 +42,8 @@ export const login = async (req, res, next) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure: true,
+      sameSite: "None",
       maxAge: 5 * 24 * 60 * 60 * 1000,
     });
 
